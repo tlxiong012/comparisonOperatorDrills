9d22db3107c17302434ebd60dc4d9b6983e7b98e
tlxiong012 <55326274+tlxiong012@users.noreply.github.com>
2019-10-05 11:42:38 -0500

    Create debugComparisonOperatorDrills-1.py

---
+++ debugComparisonOperatorDrills-1.py
@@ -0,0 +1,85 @@
+'''
+For this assignment you should look at the code created below and find the error.
+For each task, there will be one error that you must find and correct.
+Sometimes there will be an explanation of the problem and/or tips that can help you
+complete the tasks.
+EXAMPLE TASK:
+'''
+#EX)
+#  )Broken:
+#  )5 = 5
+#  )Correct:
+5 == 5
+
+'''
+END EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1)
+# )Broken: missing an "=" symbol
+# )4 = 4
+# )Correct:4==4
+
+#2)
+# )Broken:missing a "=" symbol
+# )3 = 3
+# )Correct: 3==3
+
+#3)
+# )Broken:extra space between equal signs
+# )5 = = 5
+# )Correct:5==5
+
+#4)
+# )Broken:extra space between equal signs
+# )4 = = 4
+# )Correct:4==4
+
+#5)
+# )Broken:both variables are the same
+# )a = 4
+# )a = 3
+# PROBLEM: I want to check if a is equal to 3
+# )Correct: a=4 b=3 a==b = False
+
+#6)
+# )Broken: missing a equal sign
+# )a = 4
+# )b = 3
+# )a = b
+# PROBLEM: I want to check if a is equal to b, not assign it to b
+# )Correct:a==b
+
+#7)
+# )Broken:missing "=" symbol
+# )a = 3
+# )a < 4
+# )PROBLEM: I want it to be true when a is less than OR equal to 4
+# )Correct:a<=4
+
+#8)
+# )Broken:missing "=" symbol
+# )a = 3
+# )a > 4
+# )PROBLEM: I want it to be true when a is greater than OR equal to 4
+# )Correct:a>=4
+
+#9)
+# )Broken:missing "b" and "="
+# )a = 3
+# )b = 3
+# )a < 3
+# )PROBLEM: I want it to be true when a is less than OR equal to b
+# )Correct:a<=b
+
+#10)
+# )Broken:missing"=" symbol
+# )a = 3
+# )b = 2
+# )a > b
+# )PROBLEM: I want it to be true when a is greater than OR equal to b
+# )Correct:a>=b
